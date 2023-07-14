368f2bc459843c43b049ef0832fc5d6a446865eb
Aoda <fba.gourelgnail.comt>
2023-07-14 17:54:35 +0200

    moving toastr out

--- static/src/js/backend/Elements.js
+++ static/src/js/backend/Elements.js
@@ -1,274 +1,255 @@
 class Elements {
+	constructor() {
+		this.idCAM = uuid.v4();
+		this.creator = uuid.v4(); // id of the maker
+		this.projectCAM = config.CAMproject;
+		this.defocusCAM = null;
+		this.date = new Date().getTime(); // representing the milliseconds elapsed between 1 January 1970 00:00:00 UTC and the given date
+		this.nodes = [];
+		this.connectors = [];
+		this.currentID = null;
+		this.currentNode = null;
+		this.hasSelectedNode = false;
+		this.currentConnector = null;
+		this.hasSelectedConnector = false;
+		this.readyToMove = false;
+		this.isIncoming = false;
+	}
 
-    constructor() {
-        this.idCAM = uuid.v4();
-        this.creator = uuid.v4(); // id of the maker
-        this.projectCAM = config.CAMproject;
-        this.defocusCAM = null;
-        this.date = (new Date).getTime(); // representing the milliseconds elapsed between 1 January 1970 00:00:00 UTC and the given date
-        this.nodes = [];
-        this.connectors = [];
-        this.currentID = null;
-        this.currentNode = null;
-        this.hasSelectedNode = false;
-        this.currentConnector = null;
-        this.hasSelectedConnector = false;
-        this.readyToMove = false;
-        this.isIncoming = false;
-    }
+	addElement(newElement) {
+		const kind = newElement.kind;
+		if (kind === "Node") return this.addNode(newElement);
+		if (kind === "Connector") return this.addConnector(newElement);
+	}
 
+	updateElement(kind, field, value) {
+		switch (kind) {
+			case "Node":
+				if (this.currentNode != null) {
+					this.currentNode.updateNode(field, value);
+				}
+				break;
+			case "Connector":
+				if (this.currentConnector != null) {
+					this.currentConnector.updateConnector(field, value);
+				}
+				break;
+		}
+	}
 
-    addElement(newElement) {
-        const kind = newElement.kind;
-        switch (kind) {
-            case "Node":
-                var {
-                    success, node
-                } = this.addNode(newElement);
-                break;
-            case "Connector":
-                var {
-                    success, connector
-                } = this.addConnector(newElement);
-                break;
-        }
-    }
+	deleteElement() {
+		if (this.hasSelectedNode) {
+			return this.deleteNode();
+		}
 
-    updateElement(kind, field, value) {
-        switch (kind) {
-            case "Node":
-                if (this.currentNode != null) {
-                    this.currentNode.updateNode(field, value);
-                }
-                break;
-            case "Connector":
-                if (this.currentConnector != null) {
-                    this.currentConnector.updateConnector(field, value);
-                }
-                break;
-        }
-        this.draw();
-    }
+		if (this.hasSelectedConnector) {
+			return this.deleteConnector();
+		}
+	}
 
-    deleteElement() {
-        if (this.hasSelectedNode) {
-            var success = this.deleteNode();
-        }
+	addConnector(connector) {
+		if (this.isConnectorIn(connector) == false) {
+			if (config.BidirectionalDefault) {
+				connector.setBidirectional(true);
+			}
+			this.connectors.push(connector);
+			console.log("Connector has been added.");
+			return true;
+		}
+		var connector = this.findConnector(connector);
+		connector.isActive = true;
+		return false;
+	}
 
-        if (this.hasSelectedConnector) {
-            var success = this.deleteConnector();
-        }
-        this.draw();
-    }
+	isConnectorIn(connector) {
+		const st = this.findConnector(connector);
+		if (st == undefined) return false;
+		return true;
+	}
 
-    addConnector(connector) {
-        if (this.isConnectorIn(connector) == false) {
+	findConnector(connector) {
+		const connector1 = this.connectors.filter(
+			(elt) =>
+				(elt.target === connector.target &&
+					elt.source === connector.source) ||
+				(elt.target === connector.source &&
+					elt.source === connector.target)
+		);
+		return connector1[0];
+	}
+	getConnectorById(id) {
+		for (var elt of this.connectors) {
+			if (elt.id === id) return elt;
+		}
+		return null;
+	}
 
-            if(config.BidirectionalDefault){
-                connector.setBidirectional(true);
-            }
+	deleteConnector() {
+		if (!this.currentConnector.isDeletable) {
+			console.log("This element cannot be deleted.");
+			return false;
+		}
+		this.currentConnector.deleteConnection();
+		this.unselectConnection();
 
-            this.connectors.push(connector);
-            console.log("Connector has been added.");
-            return {
-                "success": true,
-                connector
-            };
-        }
-        var connector = this.findConnector(connector);
-        connector.isActive = true;
-        return false;
-    }
+		this.currentConnector = null;
+		this.hasSelectedConnector = false;
 
-    isConnectorIn(connector) {
-        const st = this.findConnector(connector);
-        if (st == undefined) return false;
-        return true;
-    }
+		return true;
+	}
 
-    findConnector(connector) {
-        const connector1 = this.connectors.filter(elt =>
-            ((elt.target === connector.target && elt.source === connector.source) ||
-                (elt.target === connector.source && elt.source === connector.target))
-        );
-        return connector1[0];
-    }
-    getConnectorById(id) {
-        for (var elt of this.connectors) {
-            if (elt.id === id) return elt;
-        }
-        return null;
-    }
+	addNode(node) {
+		const elt = this.getNodeById(node.id);
+		if (elt != null) {
+			console.log("Already existing element.");
+			return false;
+		}
+		this.nodes.push(node);
+		console.log("Node has been added.");
+		return true;
+	}
 
-    deleteConnector() {
-        if (!this.currentConnector.isDeletable) {
-            console.log("This element cannot be deleted.");
+	deleteNode() {
+		const nodeID = this.currentNode.id;
 
-            toastr.info(languageFileOut.edw_01notDeleteConnector, languageFileOut.edw_02notDeleteConnector, {
-                closeButton: true,
-                timeOut: 2000,
-                positionClass: "toast-top-center",
-                preventDuplicates: true
-            })
-            return false;
-        }
-        this.currentConnector.deleteConnection();
-        this.unselectConnection();
+		if (!this.currentNode.isDeletable) {
+			console.log("This element cannot be deleted.");
+			return false;
+		}
 
-        this.currentConnector = null;
-        this.hasSelectedConnector = false;
+		this.connectors.forEach((connector) => {
+			if (connector.target === nodeID || connector.source === nodeID) {
+				connector.deleteConnection();
+			}
+		});
 
-        return true;
-    }
+		this.currentNode.updateNode("isActive", false);
+		this.unselectNode();
 
-    addNode(node) {
-        const elt = this.getNodeById(node.id);
-        if (elt != null) {
-            console.log("Already existing element.");
-            return false;
-        }
-        this.nodes.push(node);
-        console.log("Node has been added.");
-        return {
-            "success": true,
-            node
-        };
-    }
+		this.currentNode = null;
+		this.hasSelectedNode = false;
+		return true;
+	}
 
-    deleteNode() {
-        const nodeID = this.currentNode.id;
+	getIndex(id, kind) {
+		switch (kind) {
+			case "Node":
+				for (var index = 0; index < this.nodes.length; index++) {
+					if (this.nodes[index].id === id) return index;
+				}
+				break;
+			case "Connector":
+				for (var index = 0; index < this.connectors.length; index++) {
+					if (this.connectors[index].id === id) return index;
+				}
+		}
+		return -1;
+	}
 
-        if (!this.currentNode.isDeletable) {
-            console.log("This element cannot be deleted.");
+	selecteNode(id) {
+		const index = this.getIndex(id, "Node");
+		if (
+			this.currentNode != null &&
+			this.nodes[index].id != this.currentNode.id
+		) {
+			var connector = new ConnectorCAM();
+			connector.establishConnection(
+				this.currentNode,
+				this.nodes[index],
+				IncreaseSliderIntensity,
+				true
+			);
+			this.addElement(connector);
+			this.unselectNode();
+			return;
+		}
 
-            toastr.info(languageFileOut.edw_01notDeleteNode, languageFileOut.edw_02notDeleteNode, {
-                closeButton: true,
-                timeOut: 2000,
-                positionClass: "toast-top-center",
-                preventDuplicates: true
-            })
-            
-            return false;
-        }
+		this.hasSelectedNode = true;
+		this.currentNode = this.nodes[index];
+		this.currentID = this.currentNode.id;
+		this.currentNode.updateNode("isSelected", !this.currentNode.isSelected);
+	}
 
-        this.connectors.forEach(connector => {
-            if (connector.target === nodeID || connector.source === nodeID) {
-                connector.deleteConnection();
-            }
-        });
+	unselectNode() {
+		this.nodes.map((node) => {
+			node.updateNode("isSelected", false);
+		});
+		this.currentNode = null;
+		this.currentID = null;
+		this.hasSelectedNode = false;
+	}
 
-        this.currentNode.updateNode("isActive", false);
-        this.unselectNode();
+	getNodeById(id) {
+		for (var elt of this.nodes) {
+			if (elt.id === id) return elt;
+		}
+		return null;
+	}
 
-        this.currentNode = null;
-        this.hasSelectedNode = false;
-        return true;
-    }
+	selectConnection(id) {
+		const index = this.getIndex(id, "Connector");
+		this.currentConnector = this.connectors[index];
+		this.currentConnector.isSelected = true;
+		this.currentID = this.currentConnector.id;
+		this.hasSelectedConnector = true;
 
-    getIndex(id, kind) {
-        switch (kind) {
-            case "Node":
-                for (var index = 0; index < this.nodes.length; index++) {
-                    if (this.nodes[index].id === id) return index;
-                }
-                break;
-            case "Connector":
-                for (var index = 0; index < this.connectors.length; index++) {
-                    if (this.connectors[index].id === id) return index;
-                }
-        }
-        return -1;
-    }
+		const source = this.getNodeById(this.currentConnector.source);
+		const target = this.getNodeById(this.currentConnector.target);
 
-    selecteNode(id) {
-        const index = this.getIndex(id, "Node");
-        if (this.currentNode != null && this.nodes[index].id != this.currentNode.id) {
-            var connector = new ConnectorCAM();
-            connector.establishConnection(this.currentNode, this.nodes[index], IncreaseSliderIntensity, true);
-            this.addElement(connector);
-            this.unselectNode();
-            return;
-        }
+		target.updateNode("isConnectorSelected", true);
+		source.updateNode("isConnectorSelected", true);
+	}
 
-        this.hasSelectedNode = true;
-        this.currentNode = this.nodes[index];
-        this.currentID = this.currentNode.id;
-        this.currentNode.updateNode("isSelected", !this.currentNode.isSelected);
-    }
+	unselectConnection() {
+		const source = this.getNodeById(this.currentConnector.source);
+		const target = this.getNodeById(this.currentConnector.target);
 
-    unselectNode() {
-        this.nodes.map((node) => {
-            node.updateNode("isSelected", false)
-        })
-        this.currentNode = null;
-        this.currentID = null;
-        this.hasSelectedNode = false;
-    }
+		target.updateNode("isConnectorSelected", false);
+		source.updateNode("isConnectorSelected", false);
 
-    getNodeById(id) {
-        for (var elt of this.nodes) {
-            if (elt.id === id) return elt;
-        }
-        return null;
-    }
+		this.currentConnector.isSelected = false;
 
-    selectConnection(id) {
-        const index = this.getIndex(id, "Connector");
-        this.currentConnector = this.connectors[index];
-        this.currentConnector.isSelected = true;
-        this.currentID = this.currentConnector.id;
-        this.hasSelectedConnector = true;
+		this.currentID = null;
+		this.currentConnector = null;
+		this.hasSelectedConnector = false;
+	}
 
-        const source = this.getNodeById(this.currentConnector.source);
-        const target = this.getNodeById(this.currentConnector.target);
+	draw() {
+		draw(this);
+	}
 
-        target.updateNode("isConnectorSelected", true);
-        source.updateNode("isConnectorSelected", true);
+	importElement(element) {
+		if (element.kind === "Node") {
+			var node = new NodeCAM(
+				0,
+				"",
+				{
+					x: 0,
+					y: 0,
+				},
+				false,
+				false
+			);
+			node.setNodeImport(element);
+			console.log("A node has been imported.");
+			this.nodes.push(node);
+		}
 
-    }
+		if (element.kind === "Connector") {
+			const source = this.getNodeById(element.source);
+			const target = this.getNodeById(element.target);
+			var connector = new ConnectorCAM();
+			connector.establishConnection(
+				source,
+				target,
+				element.intensity,
+				element.agreement
+			);
+			connector.id = element.id;
+			connector.isDeletable = element.isDeletable;
 
-    unselectConnection() {
-
-        const source = this.getNodeById(this.currentConnector.source);
-        const target = this.getNodeById(this.currentConnector.target);
-
-        target.updateNode("isConnectorSelected", false);
-        source.updateNode("isConnectorSelected", false);
-
-        this.currentConnector.isSelected = false;
-
-        this.currentID = null;
-        this.currentConnector = null;
-        this.hasSelectedConnector = false;
-    }
-
-    draw() {
-        draw(this);
-    }
-
-    importElement(element) {
-
-        console.log(element);
-        if (element.kind === "Node") {
-            var node = new NodeCAM(0, "", {
-                x: 0,
-                y: 0
-            }, false, false);
-            node.setNodeImport(element);
-            console.log("A node has been imported.");
-            this.nodes.push(node);
-        }
-
-        if (element.kind === "Connector") {
-            const source = this.getNodeById(element.source);
-            const target = this.getNodeById(element.target);
-            var connector = new ConnectorCAM();
-            connector.establishConnection(source, target, element.intensity, element.agreement);
-            connector.id = element.id;
-            connector.isDeletable = element.isDeletable;
-
-            this.connectors.push(connector);
-        }
-        this.draw();
-    }
+			this.connectors.push(connector);
+		}
+		this.draw();
+	}
 }
