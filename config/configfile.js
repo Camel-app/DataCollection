ed5bc0823629fea244a80ddad2eb671273d3f000
Camel-app <camel.app@proton.me>
2024-06-25 07:37:40 +0200

    fixed api link

--- config/configfile.js
+++ config/configfile.js
@@ -44,6 +44,6 @@ console.log("config config file:", config)
 
 
 // ! not change
-const webAddress = "http://localhost:3002/api/"; // "https://drawyourminds.de/API/"; // "http://localhost:3001/"; 
+const webAddress = "https://camadministrative.vercel.app/api/"; // "https://drawyourminds.de/API/"; // "http://localhost:3001/"; 
 
 
